e3f32b41b9a2120bbd0a66fead76577c987628e2
itaybnv <itaybnv01@gmail.com>
2018-11-17 20:31:07 +0300

    menu rework (not completed)

---
+++ First Game/Assets/Scripts/FreezeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeScript : MonoBehaviour {
+    //Variables
+    Vector3 FreezePosition;
+
+
+
+    //Methods
+	// Use this for initialization
+	void Start () {
+        FreezePosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = FreezePosition;
+	}
+}
---
+++ First Game/Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour {
+    //Variables
+    public Animator transitionAnim;
+    public string sceneName;
+    //Methods
+
+    public void changeScene(string sN)
+    {
+        sceneName = sN;
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
